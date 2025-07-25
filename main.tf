e4876fb9f0b5df7e74371dfb9b9f18678bbfb13b
Soham Patil <soham.patil@goapl.com>
2025-07-25 15:40:04 +0530

    latest code return

--- main.tf
+++ main.tf
@@ -1,14 +1,7 @@
-resource "google_storage_bucket" "tf-cicd-bucket" {
-  name          = "tf-cicd-bucket"
+resource "google_storage_bucket" "tf-cicd" {
+  name          = "trf-bucket"
   location      = "US"
   force_destroy = true
   project       = "singular-style-462604-p1"
-  lifecycle_rule {
-    condition {
-      age = 3
-    }
-    action {
-      type = "Delete"
-    }
-  }
+
 }
