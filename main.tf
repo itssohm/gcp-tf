cd34d17614f4321654cae9c03b17b9eabdb30b12
Soham Patil <soham.patil@goapl.com>
2025-07-25 15:42:46 +0530

    latest code return

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "tf-cicd" {
-  name          = "trf-bucket"
+  name          = "trf-bucket-myfirstproject"
   location      = "US"
   force_destroy = true
   project       = "singular-style-462604-p1"
