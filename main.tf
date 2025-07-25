e993ba92d4cf21a14e03b249cf8dac1941c84cd1
Soham Patil <soham.patil@goapl.com>
2025-07-25 14:45:21 +0530

    initial code

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "google_storage_bucket" "my-buck" {
+  name          = "tf-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
